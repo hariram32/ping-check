2b72720fc36b5c6b9afdaf8774071a36b808dd9d
Neil Cherry <ncherry@linuxha.com>
2016-08-24 20:07:33 -0400

    updated to support the additional information of the version 2 hosts file and results files

--- ping-check.sh
+++ ping-check.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+VERSION='v1.1.2'
+
 # JSON
 # Good
 # {
@@ -16,48 +18,65 @@
 # ping each addr
 # email on failure
 EMAIL="ncherry@linuxha.com"
-LIST="ip-addr.txt"
-RESULT='ip-result.json'
+# when run from cron this will look in your home directory
+# when run from the command line, from the current directory
+LIST="${LIST-ip-addr.txt}"
+RESULTS="${RESULTS-/var/www/html/data/ip-results.json}"
 
 if [ ! -f ${LIST} ]; then
     echo "IP Address file not found: ${LIST}" #| mutt -s "Ping check: IP Address file not found" ${EMAIL}
-    echo -e "{\n  \"hosts\": [\n  ],\n  \"script:\": \"ping-check.sh\"\n}" > ip-results.json
-    #echo "{ }" > ip-results.json
+    echo -e "{\n  \"hosts\": [\n  ],\n  \"script:\": \"ping-check.sh\"\n}" > ${RESULTS}
     exit 2
 fi
 
-while read ip
+# the ip-addr.txt format
+# 1st <white space> 2nd
+# IP                Hostname
+# egrep skips lines that start with a # (comments) and blank lines
+egrep -v '^#|^$' ${LIST} | while read ip nom
 do
     result=$(ping -c 3 $ip 2>&1)
     # xc can be 0, 1 or 2
     xc=$?
     case ${xc} in
 	1)
-	    # Send email
-	    echo -e "I was unable to ping ${ip}i\n\n${result}" #| mutt -s "Ping check for ${ip} failed" ${EMAIL}
+	    # Ping failed, Send email
+	    echo -e "I was unable to ping ${nom} ${ip}i\n\n${result}" #| mutt -s "Ping check for ${ip} failed" ${EMAIL}
 	    xc=0
 	    ;;
 	0)
+	    # Ping passed
 	    xc=1
 	    ;;
 	2)
+	    # Ping failed because of other reasons (like unknown host)
 	    xc=2
 	    ;;
     esac
     if [ -n "${s}" ]; then
-	s="${s},\n    { \"ip\": \"${ip}\", \"status\": \"${xc}\" }"
+	s="${s},\n    { \"name\": \"${nom}\", \"ip\": \"${ip}\", \"status\": \"${xc}\" }"
     else
-	s="    { \"ip\": \"${ip}\", \"status\": \"${xc}\" }"
+	s="    { \"name\": \"${nom}\", \"ip\": \"${ip}\", \"status\": \"${xc}\" }"
     fi
-done < ${LIST}
-echo -e "{\n  \"hosts\": [\n${s}\n  ],\n  \"script:\": \"ping-check.sh\"\n}" > ip-results.json
+done
 
+# Sample input (ip-addr.txt)
+# 192.168.24.1	hostname1
+# 192.168.24.2	hostname2
+# 192.168.2.1	hostname3
+# failed.uucp	hostname4
+
+# send the results to the RESULTS file, in JSON format
+echo -e "{\n  \"hosts\": [\n${s}\n  ],\n  \"script:\": \"ping-check.sh ${VERSION}\"\n}" > ${RESULTS}
+
+# Sample output (ip-results.json)
 # {
 #   "hosts": [
-#     { "ip": "192.168.24.1", \"status\": "1" },
-#     { "ip": "192.168.24.2", \"status\": "1" },
-#     { "ip": "192.168.2.1", \"status\": "0" },
-#     { "ip": "failed.uucp", \"status\": "2" }
-#   ],
-#   "script:": "ping-check.sh"
+#     { "name": "hostname1", "ip": "192.168.24.1", "status": "1" },
+#     { "name": "hostname2", "ip": "192.168.24.2", "status": "1" },
+#     { "name": "hostname3", "ip": "192.168.2.1", "status": "0" },
+#     { "name": "hostname4", "ip": "failed.uucp", "status": "2" }
+#   ]
+#   "script:": "ping-check.sh v1.1.0"
 # }
+
